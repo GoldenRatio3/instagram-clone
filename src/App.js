38e2ad681f0af6376f634689f7f57e8d3690470b
Brad Winter <bradleyswinter@hotmail.co.uk>
2020-08-16 20:58:12 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Post from './Post';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [posts, setPosts] = useState([
+		{
+			username: 'Brad',
+			caption: 'Welcome!',
+			imageUrl:
+				'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+		},
+		{
+			username: 'Captain Tom',
+			caption: "A'hoy sirs",
+			imageUrl:
+				'https://cdn.bannerbuzz.co.uk/media/catalog/product/resize/560/b/o/boat-lettering.jpg',
+		},
+		{
+			username: 'Test User',
+			caption: "Hi All! I'm a test user",
+			imageUrl: 'http://s3.amazonaws.com/37assets/svn/765-default-avatar.png',
+		},
+	]);
+
+	return (
+		<div className="app">
+			<div className="app__header">
+				<img
+					className="app_headerImage"
+					src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+					alt=""
+				/>
+			</div>
+			{posts.map((post) => (
+				<Post
+					username={post.username}
+					caption={post.caption}
+					imageUrl={post.imageUrl}
+				/>
+			))}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Post.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Post.css';
+import Avatar from '@material-ui/core/Avatar';
+
+function Post({ username, caption, imageUrl }) {
+	return (
+		<div className="post">
+			<div className="post__header">
+				<Avatar className="post__avatar" alt="Bradley" />
+				<h3>{username}</h3>
+			</div>
+
+			<img class="post__image" src={imageUrl} alt="" />
+			<p className="post__text">
+				<strong>{username}</strong> {caption}
+			</p>
+		</div>
+	);
+}
+
+export default Post;
